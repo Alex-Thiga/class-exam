f40fd87c1170a6a3458b9480384d9a9a592da1e2
Alex-Thiga <loydeflacko@gmail.com>
2022-05-16 00:46:50 +0300

    add check function for questions 2,3,4  to display the marks 0 if answer chosen is wrong and 1 if answer chosen is correct

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,20 @@ function check ()
 {
     var marks = 0;
     var answer1 = document.test.qtn1.value;
-    if ( answer1 == 'nairobi' ) { marks++; }
+    var answer2 = document.test.qtn2.value;
+    var answer3 = document.test.qtn3.value;
+    var answer4 = document.test.qtn4.value;
+    if ( answer1 == 'nairobi' ) {
+        marks++;
+    }
+    if ( answer2 == "wamalwa" ) {
+      marks++;
+    }
+    if (answer3 == "1963") {
+      marks++;
+    }
+    if (answer4 == "tea") {
+      marks++;
+    }
     document.write(marks)
 }
