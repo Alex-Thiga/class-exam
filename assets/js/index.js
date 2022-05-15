ecefebc3408dcd3c3ca51640877cb276c1cedc68
Alex-Thiga <loydeflacko@gmail.com>
2022-05-16 00:12:25 +0300

    add function check to display the marks 0 if answer chosen is wrong and 1 if answer chosen is correct

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
-function evaluate ()
+function check ()
 {
-    document.write("test")
+    var marks = 0;
+    var answer1 = document.test.qtn1.value;
+    if ( answer1 == 'nairobi' ) { marks++; }
+    document.write(marks)
 }
